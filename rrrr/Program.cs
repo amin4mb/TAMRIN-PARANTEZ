5c076546331224d7ad8ab0679f3ae4458072165c
amin4mb <hosein.malekpur@gmail.com>
2022-03-13 23:38:26 +0330

    PARANTEZ
    در این تمرین ابتدا کاراکتر رو داخل لیست ریختیم ، بعد یکی یکی با استفاده از حلقه فور سوییپ کردیم اگر پرانتزی که دیده میشود رو به چپ بود حلقه متوقف میشود، اگر روبه راست بود ، اولین پرانتزی که روبه چپ را با حلقه دوم پاک میکنیم

---
+++ rrrr/Program.cs
@@ -0,0 +1,48 @@
+﻿
+var INPUT = "5(*X(5)(((*6+(54)660))6+2()))";
+
+List<char> l = INPUT.ToList();
+
+
+for (int i = 0; i < l.Count(); i++)
+{
+    if (l[i] == ')')
+    {
+        Console.WriteLine("IS NOT VALID ");
+
+        break;
+    }
+    if (l[i] == '(')
+    {
+
+        if (l.Contains(')'))
+
+        {
+            for (int j = i + 1; j < l.Count(); j++)
+            {
+                if (l[j] == ')')
+                {
+                    l.RemoveAt(j);
+                    break;
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("IS NOT VALID ");
+            break;
+
+
+        }
+       
+    }
+
+    if (i == (l.Count()) - 1)
+    {
+        Console.WriteLine("***VALID***");
+    }
+}
+
+
+
+
